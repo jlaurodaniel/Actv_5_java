aa50d7334beb04934f0b481c32b3ae5cfca92a53
Lauro Daniel Jimenez Custodio <47502588+jlaurodaniel@users.noreply.github.com>
2021-03-05 16:06:35 -0500

    Add files via upload

---
+++ actv5.java
@@ -0,0 +1,12 @@
+public class actv5 {
+
+    public static void operaciones (){
+          int num1=5,num2=3,r;
+        System.out.println("la suma de "+num1+" y el numero "+num2+" es de "+num1+num2);
+        r=num1-num2;
+        System.out.println("la resta de "+num1+" y el numero "+num2+" es de "+r);
+        System.out.println("la division de "+num1+" y el numero "+num2+" es de "+num1/num2);
+        System.out.println("la multiplicacion de "+num1+" y el numero "+num2+" es de "+num1*num2);
+    }
+
+}
